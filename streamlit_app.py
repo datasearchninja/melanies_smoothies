53ad6a7c5f4f18f30929bf2d8ce5e66027d86235
Colin Humphreys <colin@encypher.net>
2026-09-11 13:37:37 +1000

    Refactor smoothie app to include fruit nutrition info
    Removed unnecessary imports and added API call for fruit nutrition information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests  
 
 
 # Write directly to the app
@@ -10,8 +10,6 @@ st.write(
   """Choose the fruits you want in your custom Smoothie!
   """
 )
-
-import streamlit as st
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
@@ -33,7 +31,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen + ' '
-
+        st.subheader(fruit_chosen + ' Nutrion Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        sf_df =  st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -52,7 +52,5 @@ if ingredients_list:
       st.success('Your Smoothie is ordered!', icon="✅")
 
 
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df =  st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
